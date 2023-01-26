81b1be446ce31b5bd75b38d037333f1f9df20ff6
Sebastian Wicki <sebastian@isovalent.com>
2023-01-26 16:21:18 +0100

    WIP/agent: Partially implement CiliumNode status and spec update
    The way this is implemented is using a configuration flag that tells us
    what pools need to be pre-allocated.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -824,6 +824,9 @@ const (
 	// IPAM is the IPAM method to use
 	IPAM = "ipam"
 
+	// IPAMClusterPoolNodePreAlloc contains the list of IP pools which should be pre-allocated on this node
+	IPAMClusterPoolNodePreAlloc = "cluster-pool-node-pre-alloc"
+
 	// XDPModeNative for loading progs with XDPModeLinkDriver
 	XDPModeNative = "native"
 
@@ -2031,6 +2034,9 @@ type DaemonConfig struct {
 
 	// IPAM is the IPAM method to use
 	IPAM string
+
+	// IPAMClusterPoolNodePreAlloc contains the list of IP pools which should be pre-allocated on this node
+	IPAMClusterPoolNodePreAlloc map[string]string
 
 	// Enable chaining with another CNI plugin.
 	CNIChainingMode string
@@ -3325,6 +3331,11 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 		if c.EnableIPv6 {
 			c.K8sRequireIPv6PodCIDR = true
 		}
+	}
+	if m, err := command.GetStringMapStringE(vp, IPAMClusterPoolNodePreAlloc); err != nil {
+		log.Fatalf("unable to parse %s: %s", IPAMClusterPoolNodePreAlloc, err)
+	} else {
+		c.IPAMClusterPoolNodePreAlloc = m
 	}
 
 	c.KubeProxyReplacementHealthzBindAddr = vp.GetString(KubeProxyReplacementHealthzBindAddr)
